94926bc0bd15d1d3c953d4e3d8456bdda4951eac
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-12-13 10:09:45 +0530

    Update express.js

--- express.js
+++ express.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
-
-// Serve static HTML files from the public directory
 app.use(express.static('public'));
-
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
@@ -19,33 +15,6 @@ app.get('/deploy', (req, res) => {
 app.get('/support', (req, res) => {
     res.sendFile(__dirname + '/support.html');
 });
-
-// Additional routes for buttons
-app.get('/qr', (req, res) => {
-    res.send('izumi.maskser.me/qr');
-});
-
-app.get('/pair', (req, res) => {
-    res.send('izumi.maskser.me/pair');
-});
-
-app.get('/koyeb', (req, res) => {
-    res.send('koyeb.com');
-});
-
-app.get('/render', (req, res) => {
-    res.send('render.com');
-});
-
-app.get('/group', (req, res) => {
-    res.send('https://chat.whatsapp.com/KHvcGD7aEUo8gPocJsYXZe');
-});
-
-app.get('/channel', (req, res) => {
-    res.send('https://whatsapp.com/channel/0029Vaf2tKvGZNCmuSg8ma2O');
-});
-
-// Start server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
